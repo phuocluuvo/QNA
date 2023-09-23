c4cc1b48ac686b45b9d285d350ded1078e8a86f4
trongten <trongtrong099@gmail.com>
2023-09-23 17:00:12 +0700

    feat: create, paginate answer

--- backend/src/question/entity/question.entity.ts
+++ backend/src/question/entity/question.entity.ts
@@ -1,8 +1,8 @@
-import { Entity, Column, PrimaryColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Question {
-  @PrimaryColumn({ type: "uuid" })
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column({ length: 300 })
@@ -11,9 +11,9 @@ export class Question {
   @Column("text")
   content: string;
 
-  @Column()
+  @Column({ default: 0 })
   views: number;
 
-  @Column()
+  @Column({ default: 0 })
   vote: number;
 }
