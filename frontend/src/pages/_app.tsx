95dae54384c7aff1dca95705b2adb356e81ce47d
phuocluuvo <voluu0702@gmail.com>
2023-10-06 12:11:47 +0700

    add session provider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   ChakraProvider,
   ColorModeProvider,
@@ -10,36 +11,38 @@ import { appWithI18Next } from "ni18n";
 import store from "@/API/redux/store";
 import { ni18nConfig } from "../../ni18n.config";
 import "react-quill/dist/quill.snow.css";
-
+import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 import { Provider } from "react-redux";
 // @ts-ignore
-function App({ Component, pageProps }) {
+function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Provider store={store}>
-      <ChakraProvider>
-        <ColorModeProvider>
-          <ThemeProvider theme={theme}>
-            <Head>
-              <title>Question Dân It</title>
-              <meta
-                name="description"
-                content="The website answers the question about IT"
-              />
-              <link rel="icon" href="/images/favicon.ico" sizes="any" />
-              <link
-                rel="stylesheet"
-                href="//cdn.quilljs.com/1.3.6/quill.bubble.css"
-              ></link>
-              <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
-            </Head>
-            <Header />
-            <GoToTopButton />
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </ColorModeProvider>
-      </ChakraProvider>
-    </Provider>
+    <SessionProvider session={pageProps.session}>
+      <Provider store={store}>
+        <ChakraProvider>
+          <ColorModeProvider>
+            <ThemeProvider theme={theme}>
+              <Head>
+                <title>Question Dân It</title>
+                <meta
+                  name="description"
+                  content="The website answers the question about IT"
+                />
+                <link rel="icon" href="/images/favicon.ico" sizes="any" />
+                <link
+                  rel="stylesheet"
+                  href="//cdn.quilljs.com/1.3.6/quill.bubble.css"
+                ></link>
+                <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
+              </Head>
+              <Header {...pageProps} />
+              <GoToTopButton {...pageProps} />
+              <Component data-i18n-is-dynamic-list={true} {...pageProps} />
+            </ThemeProvider>
+          </ColorModeProvider>
+        </ChakraProvider>
+      </Provider>
+    </SessionProvider>
   );
 }
 export default appWithI18Next(App, ni18nConfig);
