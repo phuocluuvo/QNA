6085b8e13009f9f0c1a833319dece6ff02e0f7f3
phuocluuvo <voluu0702@gmail.com>
2023-10-25 23:39:26 +0700

    update ui

--- frontend/src/components/VoteButton/index.tsx
+++ frontend/src/components/VoteButton/index.tsx
@@ -1,6 +1,7 @@
 import { Colors } from "@/assets/constant/Colors";
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
-import { IconButton } from "@chakra-ui/react";
+import { IconButton, Tooltip } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
 import React from "react";
 type VoteButtonProps = {
   type: "up" | "down";
@@ -9,34 +10,49 @@ type VoteButtonProps = {
   size?: string | number;
 };
 function VoteButton(props: VoteButtonProps) {
+  const session = useSession();
+  let tooltipLabel = "";
+  if (!session.data?.user?.id) {
+    tooltipLabel = "*You need to login to perform the action* - ";
+  }
+  if (props.type === "up") {
+    tooltipLabel = tooltipLabel.concat(
+      "The question is showing the effort of the author, it's useful and clear"
+    );
+  } else {
+    tooltipLabel = tooltipLabel.concat("The question is unclear or not useful");
+  }
   return (
-    <IconButton
-      aria-label="Up vote"
-      onClick={props.onClick}
-      py={0}
-      my={0}
-      icon={
-        props.type === "up" ? (
-          <ChevronUpIcon
-            color={Colors(props.isDarkMode).UP_VOTE_GREEN}
-            transition={"all 0.5s"}
-            fontSize={props.size ? props.size : 30}
-            _hover={{ color: Colors(props.isDarkMode).UP_VOTE_GREEN_HOVER }}
-          />
-        ) : (
-          <ChevronDownIcon
-            color={Colors(props.isDarkMode).DOWN_VOTE_RED}
-            fontSize={props.size ? props.size : 30}
-            transition={"all 0.5s"}
-            _hover={{ color: Colors(props.isDarkMode).DOWN_VOTE_RED_HOVER }}
-          />
-        )
-      }
-      variant="ghost"
-      rounded={"full"}
-      color={Colors(props.isDarkMode).PRIMARY}
-      transition="all 0.5s"
-    />
+    <Tooltip hasArrow placement="right" label={tooltipLabel}>
+      <IconButton
+        aria-label="Up vote"
+        onClick={props.onClick}
+        py={0}
+        my={0}
+        isDisabled={!session.data?.user?.id}
+        icon={
+          props.type === "up" ? (
+            <ChevronUpIcon
+              color={Colors(props.isDarkMode).UP_VOTE_GREEN}
+              transition={"all 0.5s"}
+              fontSize={props.size ? props.size : 30}
+              _hover={{ color: Colors(props.isDarkMode).UP_VOTE_GREEN_HOVER }}
+            />
+          ) : (
+            <ChevronDownIcon
+              color={Colors(props.isDarkMode).DOWN_VOTE_RED}
+              fontSize={props.size ? props.size : 30}
+              transition={"all 0.5s"}
+              _hover={{ color: Colors(props.isDarkMode).DOWN_VOTE_RED_HOVER }}
+            />
+          )
+        }
+        variant="ghost"
+        rounded={"full"}
+        color={Colors(props.isDarkMode).PRIMARY}
+        transition="all 0.5s"
+      />
+    </Tooltip>
   );
 }
 
