8259f1f6ef3e11674e40460161a46a29710c7efb
trongten <trongtrong099@gmail.com>
2023-11-18 10:50:49 +0700

    feat: add validate tag name

--- backend/src/tag/dto/create-tag.dto.ts
+++ backend/src/tag/dto/create-tag.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+import { IsNotEmpty, IsString, Matches, MaxLength } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 import { Expose } from "class-transformer";
 
@@ -7,7 +7,7 @@ export class CreateTagDto {
   @Expose()
   @IsNotEmpty()
   @IsString()
-  @MinLength(3)
+  @Matches(/^\S*$/)
   @MaxLength(30)
   name: string;
 
