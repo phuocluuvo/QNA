ca4782b8878e93c660eddd0bc1668d9a911ad1ce
trongten <trongtrong099@gmail.com>
2023-11-18 10:02:21 +0700

    feat: add filter tag

--- backend/src/config/pagination/tag-pagination.config.ts
+++ backend/src/config/pagination/tag-pagination.config.ts
@@ -1,8 +1,11 @@
-import { PaginateConfig } from "nestjs-paginate";
+import { PaginateConfig, FilterOperator } from "nestjs-paginate";
 import { Tag } from "../../tag/entity/tag.entity";
 
 export const tagPaginateConfig: PaginateConfig<Tag> = {
   sortableColumns: ["content", "createdAt", "updatedAt"],
   defaultSortBy: [["name", "ASC"]],
   searchableColumns: ["name", "content"],
+  filterableColumns: {
+    state: [FilterOperator.EQ],
+  },
 };
